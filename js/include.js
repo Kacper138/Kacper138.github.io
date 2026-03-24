c7839d9dffff6cc488b3a45b121e991ff2ade520
Kacper Podwika <kacperpodwika@gmail.com>
2026-03-24 19:06:35 +0100

    Dodanie wyglądu PCB - szybka poprawa

--- js/include.js
+++ js/include.js
@@ -51,3 +51,21 @@ function highlightActiveLink() {
 }
 
 document.addEventListener("DOMContentLoaded", includeHTML);
+
+
+// Klikanie node'ów → przejście do podstron
+document.addEventListener("DOMContentLoaded", () => {
+  const nodes = document.querySelectorAll(".node");
+
+  nodes.forEach(node => {
+    node.addEventListener("click", () => {
+      const link = node.getAttribute("data-link");
+
+      // efekt przejścia
+      document.body.style.opacity = "0";
+      setTimeout(() => {
+        window.location.href = link;
+      }, 300);
+    });
+  });
+});
